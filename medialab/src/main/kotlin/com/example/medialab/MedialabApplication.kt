859796b68b3f7438970f6021fd9c78ea5f5c12b9
Anas-Baghdadi <anas.baghdadi@student.ehb.be>
2023-05-08 19:21:03 +0200

    newsetup

---
+++ medialab/src/main/kotlin/com/example/medialab/MedialabApplication.kt
@@ -0,0 +1,11 @@
+package com.example.medialab
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MedialabApplication
+
+fun main(args: Array<String>) {
+	runApplication<MedialabApplication>(*args)
+}
