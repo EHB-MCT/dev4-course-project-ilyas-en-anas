21ba071cf24d16d5ca05552dc57c62fed4043b15
Anas-Baghdadi <anas.baghdadi@student.ehb.be>
2023-05-10 01:52:36 +0200

    setupVuejsWithBootstrap

---
+++ frontendMedialab/medialabviews/src/router/index.js
@@ -0,0 +1,35 @@
+import {
+  createRouter,
+  createWebHistory
+} from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+import UserView from '../views/Users/View.vue'
+
+const router = createRouter({
+  history: createWebHistory(
+    import.meta.env.BASE_URL),
+  routes: [{
+      path: '/',
+      name: 'home',
+      component: HomeView
+    },
+    {
+      path: '/items',
+      name: 'items',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/ItemView.vue')
+    },
+    {
+      path: '/users',
+      name: 'users',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: UserView
+    }
+  ]
+})
+
+export default router
---
+++ frontendMedialab/medialabviews/src/views/ItemView.vue
@@ -0,0 +1,8 @@
+<template>
+<main> 
+  <h1>This is an items page</h1>
+</main>
+   
+ 
+</template>
+
